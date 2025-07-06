8a2ea9d6cfaf4688886de3a7222a6b4ec34b466e
Jidlafe Hegner <jidlafehegner@gmail.com>
2025-07-06 02:17:38 -0300

    feat: comentando no de criacao e adicionado recurso de nos

--- modules/cluster/vultr/main.tf
+++ modules/cluster/vultr/main.tf
@@ -15,20 +15,20 @@ resource "vultr_kubernetes" "labs_cluster" {
   vpc_id          = var.vpc_id
 
   # A default node pool is required when first creating the resource but it can be removed at a later point
-  node_pools {
-    node_quantity = 1
-    plan          = "vc2-1c-2gb"
-    label         = "vke-default-first-creating"
-    auto_scaler   = false
-  }
+  #node_pools {
+  #  node_quantity = 1
+  #  plan          = "vc2-1c-2gb"
+  #  label         = "vke-default-first-creating"
+  #  auto_scaler   = false
+  #}
 
 }
 
 # Create a Kubernetes cluster with a single node pool
-#resource "vultr_kubernetes_node_pools" "labs_cluster_node_pools" {
-#  cluster_id    = vultr_kubernetes.labs_cluster.id
-#  node_quantity = var.node_quantity
-#  plan          = var.plan
-#  label         = var.node_pools_label
-#  labels        = var.node_pools_labels
-#}
+resource "vultr_kubernetes_node_pools" "labs_cluster_node_pools" {
+  cluster_id    = vultr_kubernetes.labs_cluster.id
+  node_quantity = var.node_quantity
+  plan          = var.plan
+  label         = var.node_pools_label
+  labels        = var.node_pools_labels
+}
