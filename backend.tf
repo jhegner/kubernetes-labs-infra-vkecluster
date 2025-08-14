74e21de166a4862d11f93449362c10c77ba152b5
Jidlafe Hegner <jidlafehegner@gmail.com>
2025-08-13 21:39:32 -0300

    feat: alterado nome backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "kubernetes-labs-tf-backend-aws-s3bucket"
-    key            = "lab/state/tf-kubernetes-cluster"
+    key            = "lab/state/tf-infra-vkecluster"
     region         = "us-east-1"
     use_lockfile   = true
   }
