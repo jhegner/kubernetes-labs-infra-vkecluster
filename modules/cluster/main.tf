5b591d583d048c41251beec2be178170a13a0d0a
Jidlafe Hegner <jidlafehegner@gmail.com>
2025-08-17 16:58:21 -0300

    chore: simplificando versao 1 lab

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -16,23 +16,11 @@ resource "vultr_kubernetes" "labs_cluster" {
 
   # Create a default node pool for development environment
   node_pools {
-    node_quantity = var.dev_env.node_quantity
-    plan          = var.dev_env.plan
-    label         = var.dev_env.label
-    labels        = var.dev_env.labels
+    node_quantity = var.lab_env.node_quantity
+    plan          = var.lab_env.plan
+    label         = var.lab_env.label
+    labels        = var.lab_env.labels
     auto_scaler   = local.auto_scaler 
   }
 
-}
-
-# Create a a node pool for production environment in the same cluster
-resource "vultr_kubernetes_node_pools" "labs_cluster_node_pools" {
-  cluster_id    = vultr_kubernetes.labs_cluster.id
-  node_quantity = var.prod_env.node_quantity
-  plan          = var.prod_env.plan
-  label         = var.prod_env.label
-  labels        = var.prod_env.labels
-  tag           = var.prod_env.tag
-
-  depends_on = [ vultr_kubernetes.labs_cluster ]
 }
--- scripts/aws/s3-delete-statefile.sh
+++ scripts/aws/s3-delete-statefile.sh
@@ -4,5 +4,5 @@ cd ../../
 
 echo "--- Removendo o arquivo terraform de estado da infra vultr ---"
 
-aws s3 rm s3://kubernetes-labs-tf-backend-aws-s3bucket/lab/state/tf-kubernetes-cluster
-aws s3 rm s3://kubernetes-labs-tf-backend-aws-s3bucket/lab/state/tf-kubernetes-cluster.tflock
+aws s3 rm s3://kubernetes-labs-tf-backend-aws-s3bucket/lab/state/tf-infra-vkecluster
+aws s3 rm s3://kubernetes-labs-tf-backend-aws-s3bucket/lab/state/tf-infra-vkecluster.tflock
